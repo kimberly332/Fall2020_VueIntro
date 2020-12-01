a21b6a8633546ed74b46e7300c4ffedf8147a246
kimberly332 <kimberly.wang1010@gmail.com>
2020-12-01 14:38:47 -0500

    internal components

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+import { fetchData } from "./components/TheDataMiner.js";
+
 (() => {
+    Vue.component("prof-card", {
+        props: ["prof"],
+        template: `<li>
+                        <img :src="'images/' +  prof.avatar" alt="prof image">
+                        <p>Prof Name: {{ prof.name }}</p>
+
+                        <a href="" class="remove-prof">Show {{ prof.name }}'s info</a>
+                        <a href="" class="remove-prof">Remove {{ prof.name }}</a>
+                    </li>`,
+        
+        created: function() {
+            console.log(`created ${this.prof.name}'s card`);
+        }
+    });
+
     let vue_vm = new Vue({
         // link Vue to an element in our HTML
         //el: "#app",
@@ -8,21 +25,26 @@
             anotherMessage: "more text, so simple! much winning",
             removeAformat: true, 
             showBioData: false,
+            profs: [],
+            currentProfData: {}
 
-            profs: [
-                { name: "Justin", role: "coordinator", nickname: "nitsuJ" },
-                { name: "John", role: "prof", nickname: "super chill" },
-                { name: "Joe", role: "prof", nickname: "Teddy Bear" },
-            ]
+            // profs: [
+            //     { name: "Justin", role: "coordinator", nickname: "nitsuJ" },
+            //     { name: "John", role: "prof", nickname: "super chill" },
+            //     { name: "Joe", role: "prof", nickname: "Teddy Bear" },
+            // ]
         },
 
         // this is the "mounted" lifecycle hook. Vue is done creating itself, and has attached itself, and has attached itself to the "app" div on the page
         mounted: function() {
-            console.log("Vue is mounted!");
-            // alert("Hey there! your vue instance is ready");
-
-            this.profs.push({name: "Jarrod", role: "supermodel prof", nickname: "Zoolander"})
-            // alert("you added Jarrod!")
+            console.log("Vue is mounted, trying a fetch for the initial data");
+            
+            fetchData("./includes/index.php")
+                .then(data => {
+                    // push the prof data into profs array
+                    data.forEach(prof => this.profs.push(prof));
+                })
+                .catch(err => console.error(err));
         },
 
         // run a method when you change our view (update the DOM with Vue)
@@ -39,13 +61,26 @@
                 console.log("clikced on the header");
             },
 
+            showProfData(target) {
+                // remove this prof from the prods array
+                console.log("clicked to view prof bio data", target, target.name);
+                // the "this" keyword inside a vue instance REFERS to the Vue instance itself by default
+                
+                // toggle the property between true and false using a ternary statement
+                this.showBioData = this.showBioData ? false : true;
+
+                // make the selected prof's data visible
+                this.currentProfData = target;
+            },
+
             removeProf(target) {
                 // remove this prof from the prods array
                 console.log("clicked to remove prof", target, target.name);
                 // the "this" keyword inside a vue instance REFERS to the Vue instance itself by default
                 
-                // toggle the property between true and false using a ternary statement
-                this.showBioData = this.showBioData ? false : true
+                // make the selected prof's data visible
+                // this.profs.splice(this.profs.indexOf(target), 1);
+                this.$delete(this.profs, target);
             }
         }
     }).$mount("#app"); // also connects Vue to your wrapper in HTML
